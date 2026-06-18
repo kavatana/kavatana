fc54867f79ed7c646d22ea931f522553d73815f7
kavatana <kavatanaa@gmail.com>
2026-06-18 11:39:47 +0700

    fix: add PhsarOS to useProject mapping and add Visit Product button to project cards

--- composables/useProject.ts
+++ composables/useProject.ts
@@ -2,9 +2,11 @@ import type { Project } from '~/types/content'
 import bayonhub from '~/content/projects/bayonhub.json'
 import kavatanaPortfolio from '~/content/projects/kavatana-portfolio.json'
 import cyberArena from '~/content/projects/cyber-arena-team-os.json'
+import phsaros from '~/content/projects/phsaros.json'
 
 const projectsMap: Record<string, any> = {
   bayonhub,
+  phsaros,
   'kavatana-portfolio': kavatanaPortfolio,
   'cyber-arena-team-os': cyberArena
 }
